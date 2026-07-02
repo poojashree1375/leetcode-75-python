a8576dc9e4eab0fa414e01a90150be69eef55a49
Pooja Shree R <poojashreee13725@gmail.com>
2026-07-02 17:01:19 +0530

    Added solution for Merge Strings Alternatively problem

---
+++ array-string/merge-strings-alternatively.py
@@ -0,0 +1,20 @@
+# Problem: Merge Strings Alternatively
+# Topic: Array / String
+
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        i = 0
+        j = 0
+        merged = []
+        while i < len(word1) and j < len(word2):
+            merged.append(word1[i])
+            merged.append(word2[j])
+            i += 1
+            j += 1
+        while i < len(word1):
+            merged.append(word1[i])
+            i += 1
+        while j < len(word2):
+            merged.append(word2[j])
+            j += 1
+        return "".join(merged)
